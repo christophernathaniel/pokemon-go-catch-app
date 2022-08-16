f14110b9e209deb9ad0154ec7c87796628acf710
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2022-08-16 19:27:37 +0100

    Added UseState + fetchData request to PokeAPI

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Project has been created with Create React App
 SCSS has been included in this Application
 Tailwind has been included in this Application (https://tailwindcss.com/docs/guides/create-react-app)
+
+Application has been cleared of default comments.
+
+See the 3sidedcube developer test: https://github.com/3sidedcube/web-developer-test
--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => res.json())
+      .then((data) => setData(data.results));
+
+    console.log(data);
+  };
+
   return <div className="App"></div>;
 }
 
