eaf0fd1ed8675f5dbc2ced049aef870d50386413
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2022-08-16 21:57:15 +0100

    Set Pokemon State State added to hold pokemons tats

--- src/Pages/Pokemon.js
+++ src/Pages/Pokemon.js
@@ -1,14 +1,32 @@
 import { useParams } from "react-router-dom";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Pokemon = () => {
   let name = useParams();
+
+  let defaultPokeState = {
+    image: "",
+    id: null,
+    speed: "",
+    shiny: false,
+    hp: 0,
+    defence: 0,
+    attack: 0,
+    specialDefence: 0,
+    specialAttack: 0,
+    weight: 0,
+    types: [],
+  };
+
+  const [pokeStat, setPokeStat] = useState(defaultPokeState);
 
   const fetchPokemon = useCallback(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/" + name.name)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+
+        setPokeStat = [...pokeStat, data.sprites.front_default];
       });
   }); // Add Dependancies
 
@@ -20,6 +38,7 @@ const Pokemon = () => {
     <div className="poke-card">
       <h1>Pokemon</h1>
       <h2>{name.name}</h2>
+      <img src="{pokeStat.image}" />
     </div>
   );
 };
