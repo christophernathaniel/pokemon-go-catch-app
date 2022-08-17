c3cc4a05ff3da9ddb15a4e80437351aafb044e65
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2022-08-18 00:49:37 +0100

    Removed fragment using classname

--- src/Component/PokeCard.js
+++ src/Component/PokeCard.js
@@ -66,185 +66,183 @@ const PokeCard = ({ characteristic, fav, setFav, compare, setCompare }) => {
 
   return (
     <div className="poke-card">
-      <>
-        <h2 className="charactaristic-name">{characteristic.name}</h2>
-        <div className="poke-card-keyinformation">
-          <div className="types">
-            <ul className="type-list">
-              {characteristic.types.map((type, i) => (
-                <li key={i} className={type.type.name}>
-                  {type.type.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            {fav.some((item) => {
-              return item.name === characteristic.name;
-            }) && (
-              <button
-                className="poke-favorite"
-                onClick={() => pokeRemoveFavourite(characteristic)}
-              >
-                <AiFillHeart />
-              </button>
-            )}
-
-            {!fav.some((item) => {
-              return item.name === characteristic.name;
-            }) && (
-              <button
-                className="poke-favorite"
-                onClick={() => pokeFavourite(characteristic)}
-              >
-                <AiOutlineHeart />
-              </button>
-            )}
-          </div>
-        </div>
-
-        <div className="characteristic-image">
-          <img alt={characteristic.name} src={spriteKey} />
-        </div>
-        <div className="pokemon-view-options">
-          <span
-            className="sprite-front"
-            onClick={() => {
-              setSpriteKey(characteristic.sprites["front_default"]);
-            }}
-          >
-            Front
-          </span>
-          <span
-            className="sprite-back"
-            onClick={() => {
-              setSpriteKey(characteristic.sprites["back_default"]);
-            }}
-          >
-            Back
-          </span>
-          <span
-            className="sprite-shiny"
-            onClick={() => {
-              setSpriteKey(characteristic.sprites["front_shiny"]);
-            }}
-          >
-            Shiny
-          </span>
-        </div>
-
-        <div className="pokemon-stats">
-          <table className="table-fixed">
-            <tbody>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <FaWeightHanging /> Weight
-                  </span>
-                </td>
-                <td>{characteristic.weight}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiBodyHeight />
-                    Height
-                  </span>
-                </td>
-                <td>{characteristic.height}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiBroadsword />
-                    Attack
-                  </span>
-                </td>
-                <td>{characteristic.stats[4].base_stat}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <BsFillShieldFill />
-                    Defence
-                  </span>
-                </td>
-                <td>{characteristic.stats[3].base_stat}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiGooeySword />
-                    Special Attack
-                  </span>
-                </td>
-                <td>{characteristic.stats[2].base_stat}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiZebraShield />
-                    Special Defence
-                  </span>
-                </td>
-                <td>{characteristic.stats[1].base_stat}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiHealthPotion />
-                    Health Points (HP)
-                  </span>
-                </td>
-                <td>{characteristic.stats[5].base_stat}</td>
-              </tr>
-              <tr>
-                <td>
-                  <span className="statistic-icon">
-                    <GiSpeedometer />
-                    Speed
-                  </span>
-                </td>
-                <td>{characteristic.stats[0].base_stat}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div className="abilties">
-          <ul className="ability-list">
-            {characteristic.abilities.map((ability, i) => (
-              <li key={i} className={ability.ability.name}>
-                {ability.ability.name}
+      <h2 className="charactaristic-name">{characteristic.name}</h2>
+      <div className="poke-card-keyinformation">
+        <div className="types">
+          <ul className="type-list">
+            {characteristic.types.map((type, i) => (
+              <li key={i} className={type.type.name}>
+                {type.type.name}
               </li>
             ))}
           </ul>
         </div>
-
-        <div clasName="comparison-options">
-          {!compare.some((item) => {
+        <div>
+          {fav.some((item) => {
             return item.name === characteristic.name;
           }) && (
             <button
-              className="poke-comparison ui-button inactive"
-              onClick={() => pokeCompare(characteristic)}
+              className="poke-favorite"
+              onClick={() => pokeRemoveFavourite(characteristic)}
             >
-              Compare
+              <AiFillHeart />
             </button>
           )}
 
-          {compare.some((item) => {
+          {!fav.some((item) => {
             return item.name === characteristic.name;
           }) && (
             <button
-              className="poke-comparison ui-button active"
-              onClick={() => pokeRemoveCompare(characteristic)}
+              className="poke-favorite"
+              onClick={() => pokeFavourite(characteristic)}
             >
-              Uncompare
+              <AiOutlineHeart />
             </button>
           )}
         </div>
-      </>
+      </div>
+
+      <div className="characteristic-image">
+        <img alt={characteristic.name} src={spriteKey} />
+      </div>
+      <div className="pokemon-view-options">
+        <span
+          className="sprite-front"
+          onClick={() => {
+            setSpriteKey(characteristic.sprites["front_default"]);
+          }}
+        >
+          Front
+        </span>
+        <span
+          className="sprite-back"
+          onClick={() => {
+            setSpriteKey(characteristic.sprites["back_default"]);
+          }}
+        >
+          Back
+        </span>
+        <span
+          className="sprite-shiny"
+          onClick={() => {
+            setSpriteKey(characteristic.sprites["front_shiny"]);
+          }}
+        >
+          Shiny
+        </span>
+      </div>
+
+      <div className="pokemon-stats">
+        <table className="table-fixed">
+          <tbody>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <FaWeightHanging /> Weight
+                </span>
+              </td>
+              <td>{characteristic.weight}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiBodyHeight />
+                  Height
+                </span>
+              </td>
+              <td>{characteristic.height}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiBroadsword />
+                  Attack
+                </span>
+              </td>
+              <td>{characteristic.stats[4].base_stat}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <BsFillShieldFill />
+                  Defence
+                </span>
+              </td>
+              <td>{characteristic.stats[3].base_stat}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiGooeySword />
+                  Special Attack
+                </span>
+              </td>
+              <td>{characteristic.stats[2].base_stat}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiZebraShield />
+                  Special Defence
+                </span>
+              </td>
+              <td>{characteristic.stats[1].base_stat}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiHealthPotion />
+                  Health Points (HP)
+                </span>
+              </td>
+              <td>{characteristic.stats[5].base_stat}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="statistic-icon">
+                  <GiSpeedometer />
+                  Speed
+                </span>
+              </td>
+              <td>{characteristic.stats[0].base_stat}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="abilties">
+        <ul className="ability-list">
+          {characteristic.abilities.map((ability, i) => (
+            <li key={i} className={ability.ability.name}>
+              {ability.ability.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="comparison-options">
+        {!compare.some((item) => {
+          return item.name === characteristic.name;
+        }) && (
+          <button
+            className="poke-comparison ui-button inactive"
+            onClick={() => pokeCompare(characteristic)}
+          >
+            Compare
+          </button>
+        )}
+
+        {compare.some((item) => {
+          return item.name === characteristic.name;
+        }) && (
+          <button
+            className="poke-comparison ui-button active"
+            onClick={() => pokeRemoveCompare(characteristic)}
+          >
+            Uncompare
+          </button>
+        )}
+      </div>
     </div>
   );
 };
