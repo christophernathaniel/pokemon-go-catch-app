6ec29474247a7584a4fa24be69761efffe07eebd
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2022-08-21 15:49:01 +0100

    Selector modified to show modified state

--- src/Pages/Battle.js
+++ src/Pages/Battle.js
@@ -1,24 +1,26 @@
 /* Currently Under Construction */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Battle = ({ fav, setFav }) => {
-  const [pokemonSelection, setPokemonSelection] = useState([]);
+  const [pokemonSelection, setPokemonSelection] = useState([0, 1]);
   const [selection, setSelection] = useState(1);
 
   console.log(fav);
 
+  // Manage the Pokemon List
   function PokemonList({ fav, selection }) {
+    // Manage the Pokemon Selection
+    // uses Selection to determine which Pokemon is selected
+    // Adds to the pokemonSelection array
     function makeSelection(item, selection) {
       let i = selection === 1 ? 0 : 1;
-      let items = pokemonSelection;
+      let items = pokemonSelection.slice(); // Slice used to copy the array and show render change
       items[i] = item;
-
       setPokemonSelection(items);
-
-      console.log(pokemonSelection);
     }
 
+    // Output component for Pokemon List
     return fav.map((item, index) => {
       return (
         <p onClick={() => makeSelection(item, selection)} key={item.name}>
@@ -28,22 +30,27 @@ const Battle = ({ fav, setFav }) => {
     });
   }
 
-  function Selection() {
-    return (
-      <div className="selection-container">
-        <div className="selection" onClick={() => setSelection(1)}>
-          One
+  // Change the Selection
+  function Selection({ pokemonSelection }) {
+    return pokemonSelection.map((item, index) => {
+      // Each loop increases its setSelection count
+      let count = index + 1;
+      return (
+        <div
+          onClick={() => {
+            setSelection(count);
+          }}
+        >
+          test
+          {item.name}
         </div>
-        <div className="selection" onClick={() => setSelection(2)}>
-          Two
-        </div>
-      </div>
-    );
+      );
+    });
   }
 
   return (
     <div>
-      <Selection />
+      <Selection pokemonSelection={pokemonSelection} />
       <PokemonList fav={fav} selection={selection} />
     </div>
   );
